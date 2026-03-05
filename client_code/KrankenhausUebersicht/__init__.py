fb720a1136329170717c3ca9c92887d1b95c7b77
tobias.reichart@student.htl-rankweil.at <tobias.reichart@student.htl-rankweil.at>
2026-03-05 11:29:57 +0000

    Edited AAA_UebersichtsSeite and KrankenhausUebersicht

--- client_code/KrankenhausUebersicht/__init__.py
+++ client_code/KrankenhausUebersicht/__init__.py
@@ -11,12 +11,28 @@ class KrankenhausUebersicht(KrankenhausUebersichtTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.  
+    # Any code you write here will run before the form opens. 
+    self.layout.set_event_handler('drop_down_krankenhaus_has_changed', self.drop_down_krankenhaus_has_changed)
 
   @handle("", "show")
   def form_show(self, **event_args):
     self.layout.reset_links()
     self.layout.link_krankenhaus.role = 'selected'
-  
+
   def drop_down_krankenhaus_has_changed(self, **event_args):
-    print("so fucking tuff")
+    """This method is called when the drop down element has changed"""
+    try:
+      print(self.layout.drop_down_krankenhaus.selected_value)
+      coordinates = anvil.server.call('get_coordinates', self.layout.drop_down_krankenhaus.selected_value)[0]
+      print(coordinates)
+  
+      marker = GoogleMap.Marker(
+        animation=GoogleMap.Animation.DROP,
+        position=GoogleMap.LatLng(coordinates[0], coordinates[1])
+      )
+  
+      self.map_krankenhaus.zoom = 20
+      self.map_krankenhaus.center = GoogleMap.LatLng(coordinates[0], coordinates[1])
+      self.map_krankenhaus.add_component(marker)
+    except:
+      pass
