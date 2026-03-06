cdc6de51ab3c926b0d984ef83296dfd60263262f
tobias.reichart@student.htl-rankweil.at <tobias.reichart@student.htl-rankweil.at>
2026-03-06 08:04:36 +0000

    Edited StationenUebersicht, AAA_UebersichtsSeite, Backend and KrankenhausUebersicht

--- server_code/Backend.py
+++ server_code/Backend.py
@@ -14,8 +14,15 @@ def get_krankenhaeuser():
   return result
 
 @anvil.server.callable
-def get_coordinates(krankenhausname: str):
+def get_coordinates(krankenhaus_name: str):
   with sqlite3.connect(data_files["krankenhaus.db"]) as conn:
     cur = conn.cursor()
-    result = cur.execute(f"SELECT latitude, longitude FROM krankenhaus WHERE name = '{krankenhausname}'").fetchall()
+    result = cur.execute(f"SELECT latitude, longitude FROM krankenhaus WHERE krankenhaus_id = '{krankenhaus_name}'").fetchall()
   return result
+
+@anvil.server.callable
+def get_station_info(krankenhaus_name: str):
+  with sqlite3.connect(data_files["krankenhaus.db"]) as conn:
+    cur = conn.cursor()
+    result = cur.execute(f"SELECT f.bezeichnung AS stationenfachrichtung, COUNT(z.zimmer_id) AS zimmeranzahl FROM station s JOIN fachrichtung f ON s.fachrichtung_id = f.fachrichtung_id JOIN zimmer z on z.station_id = s.station_id JOIN krankenhaus k on k.krankenhaus_id = s.krankenhaus_id WHERE k.name = {krankenhaus_name} GROUP BY s.station_id;").fetchtall()
+  return [dict(row) for row in result]
