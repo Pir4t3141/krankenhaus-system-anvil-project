b33dfab9632fedb2e6df053ab9e6c0d58b34040d
tobias.reichart@student.htl-rankweil.at <tobias.reichart@student.htl-rankweil.at>
2026-03-11 10:43:19 +0000

    Edited Backend

--- server_code/Backend.py
+++ server_code/Backend.py
@@ -28,3 +28,12 @@ def get_station_info(krankenhaus_name: str):
     result = cur.execute(f"SELECT f.bezeichnung AS stationenfachrichtung, COUNT(z.zimmer_id) AS zimmeranzahl FROM station s JOIN fachrichtung f ON s.fachrichtung_id = f.fachrichtung_id JOIN zimmer z ON z.station_id = s.station_id JOIN krankenhaus k ON k.krankenhaus_id = s.krankenhaus_id WHERE k.name = '{krankenhaus_name}' GROUP BY s.station_id;").fetchall()
     print(result)  
   return [dict(row) for row in result]
+
+@anvil.server.callable
+def get_einzelne_stationen_info(krankenhaus_name: str, station_id: int):
+  with sqlite3.connect(data_files["krankenhaus.db"]) as conn:
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    result = cur.execute(f"SELECT s.station_id AS station_id, f.bezeichnung AS stationenfachrichtung, stats.zimmeranzahl, stats.bettenanzahl, COUNT(b.belegt_id) AS alle_belegungen, COUNT(CASE WHEN b.ende IS NULL THEN b.belegt_id END) AS aktuelle_belegungen, ROUND(CAST((COUNT(CASE WHEN b.ende IS NULL THEN b.belegt_id END))*100 AS DOUBLE)/stats.bettenanzahl,2) AS prozent FROM station s JOIN fachrichtung f ON s.fachrichtung_id = f.fachrichtung_id JOIN krankenhaus k ON k.krankenhaus_id = s.krankenhaus_id JOIN (SELECT station_id, COUNT(zimmer_id) AS zimmeranzahl, SUM(Bettenanzahl) AS bettenanzahl FROM zimmer GROUP BY station_id ) stats ON s.station_id = stats.station_id LEFT JOIN zimmer z ON z.station_id = s.station_id LEFT JOIN belegt b ON b.zimmer_id = z.zimmer_id WHERE k.name = '{krankenhaus_name}' AND s.station_id = {station_id} GROUP BY s.station_id, f.bezeichnung, stats.zimmeranzahl, stats.bettenanzahl;").fetchall()
+    print(result)  
+  return [dict(row) for row in result]
