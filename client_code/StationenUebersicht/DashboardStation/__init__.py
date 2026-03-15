5f0a13a3f5d17da7ada43f08c542c8ae0081b3b1
tobias.reichart@student.htl-rankweil.at <tobias.reichart@student.htl-rankweil.at>
2026-03-15 16:43:32 +0000

    Created StationenUebersicht.DashboardStation_Aerzte.RowTemplate2 and StationenUebersicht.DashboardStation_Aerzte; Edited Backend and StationenUebersicht.DashboardStation

--- client_code/StationenUebersicht/DashboardStation/__init__.py
+++ client_code/StationenUebersicht/DashboardStation/__init__.py
@@ -35,3 +35,23 @@ class DashboardStation(DashboardStationTemplate):
     self.plot_zimmerbelegung.data = go.Pie(
       labels=["Belegt", "Frei"],
       values=[return_values["aktuelle_belegungen"], return_values["bettenanzahl"]-return_values["aktuelle_belegungen"]])
+
+  @handle("plot_personal", "show")
+  def plot_personal_show(self, **event_args):
+    """This method is called when the Plot is shown on the screen"""
+    return_values = anvil.server.call('get_arzt_betreuer_per_station', self.row_dict["station_id"])[0]
+    print(return_values)
+    self.plot_personal.layout = {
+      'title': {'text': 'Personalverteilung'}
+    }
+    self.plot_personal.data = go.Pie(
+      labels=["Ärzte", "Pfleger"],
+      values=[return_values["anzahl_aerzte"], return_values["anzahl_pfleger"]])
+
+  @handle("plot_personal", "click")
+  def plot_personal_click(self, points, **event_args):
+    """This method is called when a data point is clicked."""
+    point_number = points[0]["point_number"]
+    if point_number == 0:
+      open_form('StationenUebersicht.DashboardStation_Aerzte', self.row_dict)
+      
---
+++ client_code/StationenUebersicht/DashboardStation_Aerzte/__init__.py
@@ -0,0 +1,33 @@
+from ._anvil_designer import DashboardStation_AerzteTemplate
+from anvil import *
+import plotly.graph_objects as go
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class DashboardStation_Aerzte(DashboardStation_AerzteTemplate):
+  row_dict = None
+
+  def __init__(self, row_dict, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+    self.label_aerzte.text = (
+      f"{row_dict['krankenhausname']} | {row_dict['stationenfachrichtung']} | Ärzte"
+    )
+    self.row_dict = row_dict
+
+  @handle("link_stationUebersicht", "click")
+  def link_stationenUebersicht_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("StationenUebersicht.DashboardStation", self.row_dict)
+
+  @handle("data_grid_aerzte_info", "show")
+  def data_grid_aerzte_info_show(self, **event_args):
+    """This method is called when the data grid is shown on the screen"""
+    return_value = anvil.server.call('get_aerzte_stats', self.row_dict["station_id"])
+    print(return_value)
+    self.repeating_panel_aerzte_info.items = return_value
